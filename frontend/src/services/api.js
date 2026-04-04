f818d46c6ddb502c6b4ba15ad021518919d369dd
abhishek80902 <kumarabhishek80902@gmail.com>
2026-04-04 10:38:27 +0530

    add root route + cors fix

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@ const API = axios.create({
   baseURL: "https://mini-project-umuy.onrender.com/api"
 });
 
-API.interceptors.request.use((req) => {
+API.interceptors.request.use((req) => { 
   const token = localStorage.getItem("token");
   if (token) req.headers.Authorization = `Bearer ${token}`;
   return req;
