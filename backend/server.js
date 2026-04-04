599e58ff2242b280878f9dab8c2a17ea58172d91
abhishek80902 <kumarabhishek80902@gmail.com>
2026-04-04 10:06:56 +0530

    File changed

--- backend/server.js
+++ backend/server.js
@@ -14,4 +14,8 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/posts', require('./routes/postRoutes'));
 
-app.listen(5000, () => console.log("Server running on 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
